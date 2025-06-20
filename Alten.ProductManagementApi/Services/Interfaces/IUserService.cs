377a9e620eb1abe65fa71b2a2b0958151a649949
Michael MABOU <michaelmabou@outlook.com>
2025-06-20 21:55:30 +0200

    beguin unit test

--- Alten.ProductManagementApi/Services/Interfaces/IUserService.cs
+++ Alten.ProductManagementApi/Services/Interfaces/IUserService.cs
@@ -5,6 +5,7 @@ namespace Alten.ProductManagementApi.Services.Interfaces;
 public interface IUserService
 {
     Task<User> CreateUserAsync(User user);
+    //Task<User?> GetUserByIdAsync(int id);
     Task<User?> GetUserByEmailAsync(string email);
     Task<(bool IsSuccess, User? User)> ValidateUserCredentialsAsync(string email, string password);
 }
